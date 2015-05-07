2eed305b0a40f0535ae86bc573cea9b8eb7b8149
raido-ddr <raido.ddr@gmail.com>
2015-05-07 22:38:55 +0300

    Optimize imports

--- src/test/com/raido/rental/pooltest/DbConnectionPoolTest.java
+++ src/test/com/raido/rental/pooltest/DbConnectionPoolTest.java
@@ -1,14 +1,8 @@
 package test.com.raido.rental.pooltest;
 
 
-import com.raido.rental.dao.UserDao;
-import com.raido.rental.dao.exception.DaoException;
-import com.raido.rental.dao.factory.DaoFactory;
 import com.raido.rental.dao.pool.ConnectionPool;
 import com.raido.rental.dao.pool.exception.ConnectionPoolException;
-import com.raido.rental.entity.User;
-import com.raido.rental.logic.ResourceName;
-import com.raido.rental.logic.util.resourcemanager.MessageBundle;
 import org.junit.*;
 import org.junit.rules.Timeout;
 
